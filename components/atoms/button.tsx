9098352f8384f358dc5501703380b97e64586fee
Hermes <adanhermes23@gmail.com>
2023-03-23 12:03:09 -0600

    button created

---
+++ components/atoms/button.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import  styled  from 'styled-components';
+import { theme } from '@/theme/theme';
+
+
+export interface ButtonProps {
+    label: string;
+    onClick: () => void;
+    size?: 'small' | 'medium' | 'large';
+    variant?: 'primary' | 'secondary' | 'tertiary';
+}
+
+type StyledButtonProps = Omit <ButtonProps, 'label'>;
+
+
+export const Button: React.FC<ButtonProps> = ({ label, onClick, size = 'medium', variant = 'primary' }) => {
+    return (
+        <StyledButton size={size} variant={variant} onClick={onClick}>
+            {label}
+        </StyledButton>
+    );
+};
+
+
+const StyledButton = styled.button<StyledButtonProps>` 
+    width: ${(props) => {
+        if (props.size === 'small') {
+            return '80px';
+        } else if (props.size === 'medium') {
+            return '100px';
+        } else {
+            return '120px';
+        }
+    }};
+    
+    height: ${(props) => { 
+        if (props.size === 'small') {
+            return '30px';
+        } else if (props.size === 'medium') {
+            return '40px';
+        } else {
+            return '50px';
+        }
+    }};
+
+    background-color: ${(props) => {
+        if (props.variant === 'primary') {
+            return theme.colors.primary;
+        }
+        else if (props.variant === 'secondary') {
+            return theme.colors.secondary;
+        }
+        else if (props.variant === 'tertiary') {
+            return theme.colors.tertiary;
+        }
+        return theme.colors.primary;
+    }};
+    
+    color: ${(props) => {
+        if (props.variant === 'primary') {
+            return theme.colors.tertiary;
+        }
+        else if (props.variant === 'secondary') {
+            return theme.colors.primary;
+        }
+        else if (props.variant === 'tertiary') {
+            return theme.colors.primary;
+        }     
+        return theme.colors.tertiary;
+    }};
+    
+    font-size: ${(props)=> {
+        if (props.size === 'small') {
+            return '14px';
+        } else if (props.size === 'medium') {
+            return '16px';
+        } else {
+            return '18px';
+        }
+    }};
+    padding: 10px 16px;
+    border-radius: 6px;
+    border: none;
+    cursor: pointer;
+`
---
+++ theme/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+    colors: {
+        primary: '#0876DD',
+        secondary: '#F5F5F5',
+        tertiary: '#FFFFFF'
+    },
+}
